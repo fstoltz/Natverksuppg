53aedaa84bb51c617ab82e8b7ab1569e3bf232f4
fstoltz <fredrik.stoltz@live.com>
2018-04-25 16:09:20 +0200

    tried deploying this to huerty.com and this setup worked fine. This commit has hardcoded IP-addresses, and is suitable for "production-use", not for local dev. Just want a snapshot of this. I used this .war file to upload to huerty and it worked.

--- ServerSide/WebService/WebService_/src/java/ServerSide/EndpointUpdater.java
+++ ServerSide/WebService/WebService_/src/java/ServerSide/EndpointUpdater.java
@@ -17,7 +17,7 @@ public class EndpointUpdater implements Runnable{
     
     synchronized public String getCurrentSQLValues() throws SQLException, ClassNotFoundException{
         Class.forName("com.mysql.jdbc.Driver");
-        Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/sensorlogs?autoReconnect=true&useSSL=false", "root", "nacka17");
+        Connection con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/sensorlogs?autoReconnect=true&useSSL=false", "iot17", "nackademin");
         Statement stmt = (Statement) con.createStatement();
         ResultSet rs = stmt.executeQuery("SELECT * FROM livetempdata;");
         
@@ -51,7 +51,24 @@ public class EndpointUpdater implements Runnable{
     public void run() {
         try {
             while(true){
+                /*What we could potentially do here is that instead of querying the databse for latest values,
+                we have a serverSocket listening for input from the sensors DIRECTLY, raw, realtime.. interesting
+                maybe all the endpointupdaters can share this same socket thingy, and that 'master' reads in updates
+                from all feathers, and as soon as one feather sends something, this master return that name and data
+                not sure. instead of having .sleep for 3 seconds, we could maybe integrate a .readLine from an inputstream
+                and this inputstream is like a master inputstream, that a master object holds, and as soon as the master
+                gets a .readLine from one of the feathers, it delivers this to all of these endpointupdaters and
+                then it gets pushed out to the individual client. This setup might make it more real-time, since all
+                connected browsers ought to get the same update at the same time, more or less.
+                And so when a feather sends something to the dataserver, all that happens is that those values enter the
+                historical table, and do not bother with a livetempdata table, since the live values are pushed
+                directly to connected clients.*/
+                /*More and more I'm thinking we might not even need two separate "Server" projects. Maybe we can integrate
+                the datacollectorServer as a thread into this program instead??? and that will be what I above called the 'Master'
+                that reads input from all sensors and forwards that to all the endpointupdaters that are listening.*/
+                //gets the latest values from the livetempdata table and sends it to this client
                 this.session.getBasicRemote().sendObject(this.getCurrentSQLValues());
+                //sleeps for 3 seconds before sending another update to this client
                 Thread.sleep(3000);
             }
         } catch (Exception e) {
--- ServerSide/WebService/WebService_/web/websocket.js
+++ ServerSide/WebService/WebService_/web/websocket.js
@@ -1,7 +1,7 @@
 var ws;
 
 function connect(){
-    ws = new WebSocket("ws://localhost:8080/WebService_/panel");
+    ws = new WebSocket("ws://2.248.81.16:8080/WebService_/panel");
     
     ws.onmessage = function(event) {
         console.log(event.data);
