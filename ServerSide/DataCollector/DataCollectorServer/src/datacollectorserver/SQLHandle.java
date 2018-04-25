d38b75aed4903f732ec136bc2633fbeadcd43b95
fstoltz <fredrik.stoltz@live.com>
2018-04-25 10:18:18 +0200

    Wrote some code for parseInput and tested it with some strings, works fine. Java splits the code into name / value, and the inserts these values into the MySQL database.

--- ServerSide/DataCollector/DataCollectorServer/src/datacollectorserver/SQLHandle.java
+++ ServerSide/DataCollector/DataCollectorServer/src/datacollectorserver/SQLHandle.java
@@ -2,6 +2,7 @@ package datacollectorserver;
 
 
 import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
 import java.net.*;
 import java.util.*;
 import java.io.*;
@@ -22,14 +23,39 @@ public class SQLHandle {
     
     public SQLHandle() throws SQLException{
         //Setup the connection to the MySQL Server
-        this.con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/climatelogs", "root", "nacka17");
+        this.con = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/sensorlogs", "root", "nacka17");
     }
     
     //Synchronized so there are no collisions between Giver threads
-    synchronized public void parseInput(String giverString){
+    synchronized public void parseInput(String giverString) throws SQLException{
         
+        
+        boolean pastColon = false;
+        String nameOfSender = "";
+        String temperatureVal = "";
+        for(int i = 0; i < giverString.length(); i++){
+            char c = giverString.charAt(i);
+            if(c == ':'){
+                pastColon = true;
+                continue;
+            } else if(pastColon == true){
+                temperatureVal += c;
+            } else {
+                nameOfSender += c;
+            }
+        }
+        
+        System.out.println("Name of Sender: " + nameOfSender);
+        System.out.println("Value: " + temperatureVal);
+        
+        Statement stmt = (Statement) con.createStatement();
+        stmt.executeUpdate("INSERT INTO `sensorlogs`.`tempdata` (`name`, `value`) VALUES ('"+nameOfSender+"', '"+temperatureVal+"');");
     }
     
-    
+    public static void main(String[] args) throws SQLException{
+        SQLHandle sql = new SQLHandle();
+        sql.parseInput("Fredrik:21.36");
+        sql.parseInput("Kevin:89.3112");
+    }
     
 }
