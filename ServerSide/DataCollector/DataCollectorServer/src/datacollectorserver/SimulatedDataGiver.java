e5a95f6d32622a59d1f958b7e77dc6dda475a839
fstoltz <fredrik.stoltz@live.com>
2018-04-26 19:22:08 +0200

    minor fixes for simulated device

--- ServerSide/DataCollector/DataCollectorServer/src/datacollectorserver/SimulatedDataGiver.java
+++ ServerSide/DataCollector/DataCollectorServer/src/datacollectorserver/SimulatedDataGiver.java
@@ -4,19 +4,26 @@ package datacollectorserver;
 import java.net.*;
 import java.util.*;
 import java.io.*;
+import java.util.concurrent.TimeUnit;
 
 public class SimulatedDataGiver {
     Socket socketToServer;
     PrintWriter out;
+    Random rand;
     
     public SimulatedDataGiver() throws UnknownHostException, IOException{
         this.socketToServer = new Socket(InetAddress.getByName("huerty.com"), 44321);
         this.out = new PrintWriter(socketToServer.getOutputStream());
+        this.rand = new Random();
     }
     
     public void startGeneratingData() throws InterruptedException{
-        out.println("Simulated:37.37");
-        Thread.sleep(5000);
+        while(true){
+            float dbl = rand.nextFloat();
+            out.println("SimulatedFred:"+String.valueOf(dbl));
+            out.flush();
+            Thread.sleep(5000);
+        }
     }
     
     
