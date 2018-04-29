005afe466e11e59552f0f76c25476ea290f49e52
fstoltz <fredrik.stoltz@live.com>
2018-04-29 16:26:30 +0200

    catching socketexception in master to avoid Broken pipes crashing the application *testing*

--- ServerSide/DataCollector/DataCollectorServer/src/datacollectorserver/Master.java
+++ ServerSide/DataCollector/DataCollectorServer/src/datacollectorserver/Master.java
@@ -20,13 +20,41 @@ public class Master {
     
     //added synchronized because several object might calll this method at the same time
     public void sendToEveryone(String clientString) throws IOException{
-        int count = 0;
-        for (ObjectOutputStream out : this.listOfOutStreams) {
-            //System.out.println("clientString: " + clientString);
-            out.writeObject(clientString);
-            out.flush();
-            count++;
-        }
+        ObjectOutputStream tmp = null;
+        try {
+            int count = 0;
+            
+            for (ObjectOutputStream out : this.listOfOutStreams) {
+                //System.out.println("clientString: " + clientString);
+                tmp = out; //keep track of the outstream incase a exception is thrown
+                out.writeObject(clientString);
+                out.flush();
+                count++;
+            }
         System.out.println("> Sent a message to " + count + " streams.\n");
+        } catch (SocketException se) { //Incase a Broken Pipe Socket Exception is generated, we assume it's because the browser client has closed the connection and this outstream is not 'active' anymore
+            se.printStackTrace();
+            this.listOfOutStreams.remove(tmp); //remove this outstream from the list because it's no longer 'active'
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
+
+
+
+/*
+
+java.net.SocketException: Broken pipe (Write failed)
+        at java.net.SocketOutputStream.socketWrite0(Native Method)
+        at java.net.SocketOutputStream.socketWrite(SocketOutputStream.java:111)
+        at java.net.SocketOutputStream.write(SocketOutputStream.java:155)
+        at java.io.ObjectOutputStream$BlockDataOutputStream.drain(ObjectOutputStream.java:1877)
+        at java.io.ObjectOutputStream$BlockDataOutputStream.flush(ObjectOutputStream.java:1822)
+        at java.io.ObjectOutputStream.flush(ObjectOutputStream.java:719)
+        at datacollectorserver.Master.sendToEveryone(Master.java:27)
+        at datacollectorserver.DataGiverHandle.run(DataGiverHandle.java:60)
+        at java.lang.Thread.run(Thread.java:748)
+*/
