eaba728cd7eb350e35641fe8b1d1c7d40ef02edf
fstoltz <fredrik.stoltz@live.com>
2018-05-03 20:14:41 +0200

    fixed code for analog temp-sensor /hampa

--- FeatherSide/feathercode/feathercode.ino
+++ FeatherSide/feathercode/feathercode.ino
@@ -1,13 +1,25 @@
 #include <ESP8266WiFi.h>
 
-#define ssid  "anton"
-#define pwd  "nackademin"
+#define ssid  "TeliaGateway9C-97-26-57-05-5B"
+#define pwd  "226DFC4358"
 WiFiClient client;
 int listeningPort = 44321;
 IPAddress connect_2(2,248,81,16);
-String namn = "Fredrik";  //ÄNDRA TILL DITT EGET NAMN//
+String namn = "Fredrik_2";  //ÄNDRA TILL DITT EGET NAMN//
 // initializes or defines the output pin of the LM35 temperature sensor
 int outputpin= A0;
+
+
+bool try2Connect(){
+  if(client.connect("huerty.com",listeningPort)){
+    Serial.print("Du ar nu ansluten till: ");
+    Serial.print(connect_2);
+    return true;
+  }
+  return false;
+  
+}
+
 
 //this sets the ground pin to LOW and the input voltage pin to high
 void setup(){
@@ -21,65 +33,50 @@ void setup(){
   }
   Serial.println("Du ar nu ansluten till natverket!");
   
-  if(client.connect("huerty.com",listeningPort)){
-    Serial.print("Du ar nu ansluten till: ");
-    Serial.print(connect_2);
+ while(!try2Connect()){
+    delay(2000);
+    Serial.println("trying igen....");  
   }
+  Serial.println(client.connected());
+}
   
   
-}
 
-
-void connectToServer(){
-  boolean status = false;
-  while(status == false){
-    Serial.println("Trying to connect...");
-    if((client.connect("huerty.com",listeningPort)) == true){
-      status = true;
-      Serial.println("Connected again!");
-    }
-    delay(5000);
-  }
-  /*while(!client.connect("huerty.com",listeningPort)){
-    delay(8000);
-    Serial.println("Loopar runt...");
-  }
-  Serial.println("Kopplade upp mig!");*/
-}
-
-void retryWifi(){
-  Serial.println("Attempting to reconnect to WiFi.");
-  WiFi.begin(ssid,pwd);
-  while(WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
-    delay(1000);
-  }
-}
 
 void loop() {
-  client.read();
-  Serial.println("start of loop");
-  Serial.println(client.connected());
-if((client.connected()) == false || WiFi.status() != WL_CONNECTED){
-  retryWifi();
-  connectToServer();
-} else {
+if(client.status()&& WiFi.status() == WL_CONNECTED){
   int analogValue = analogRead(outputpin);
   float millivolts = (analogValue/1024.0) * 3300; //3300 is the voltage provided by NodeMCU
   float celsius = millivolts/10;
-  Serial.print("in DegreeC=   ");
-  Serial.println(celsius);
-  
-  //---------- Here is the calculation for Fahrenheit ----------//
-  
-  float fahrenheit = ((celsius * 9)/5 + 32);
-  //Serial.print(" in Farenheit=   ");
-  //Serial.println(fahrenheit);
   digitalWrite(LED_BUILTIN, LOW);
   client.println(namn+":"+celsius);
   digitalWrite(LED_BUILTIN, HIGH);
-  boolean val = client.connected();
-  Serial.println(val);
+  //Serial.println(val);
   delay(2000);
   }
+else{
+     if(WiFi.status() != WL_CONNECTED){
+         WiFi.begin(ssid,pwd);
+         Serial.println(".");
+        while(WiFi.status() != WL_CONNECTED){
+            Serial.print(".");
+            delay(500);
+          }
+        Serial.println("WiFi uppe igen!");
+      }
+    if(!client.status()){
+      Serial.println("ANSLUTER IGEN");
+      while(!try2Connect()){
+        delay(2000);
+        Serial.println("trying igen....");  
+      } 
+  }
 }
+
+}
+
+
+
+  
+
+
